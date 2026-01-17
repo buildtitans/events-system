8128f8a5e9746434ff424c724983776832be12a8
Trent Irvin <t.m.irvin1997@gmail.com>
2026-01-17 15:38:43 -0600

    removing more conflict markers

--- src/lib/hooks/auth/useLogin.ts
+++ src/lib/hooks/auth/useLogin.ts
@@ -1,107 +1,64 @@
-"use client"
-<<<<<<< HEAD
-import { useEffect, useState } from "react"
-=======
-import { useEffect } from "react"
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
-import { trpcClient } from "@/src/trpc/trpcClient"
-import { useDispatch, useSelector } from "react-redux"
-import { loginSuccess } from "@/src/lib/store/slices/AuthSlice"
-import { AppDispatch, RootState } from "@/src/lib/store"
-import { LoginCredentials } from "../useValidateCredentialsInput"
-import type { UseLoginHook } from "../../types/types"
+"use client";
+
+import { useEffect } from "react";
+import { trpcClient } from "@/src/trpc/trpcClient";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "@/src/lib/store/slices/AuthSlice";
+import type { AppDispatch, RootState } from "@/src/lib/store";
+import type { LoginCredentials } from "../useValidateCredentialsInput";
+import type { UseLoginHook } from "../../types/types";
 import { useRouter } from "next/navigation";
-import { AuthenticationSchemaType } from "@/src/schemas/loginCredentialsSchema"
-<<<<<<< HEAD
-import { currentLoginStatus } from "../../store/slices/RenderingSlice"
+import type { AuthenticationSchemaType } from "@/src/schemas/loginCredentialsSchema";
+import { enqueueSnackbar } from "../../store/slices/RenderingSlice";
 
 const useLogin = (credentials: LoginCredentials): UseLoginHook => {
     const userKind = useSelector((s: RootState) => s.auth.userKind);
-    const loginStatus = useSelector((s: RootState) => s.rendering.loginStatus);
-=======
-import { enqueueSnackbar } from "../../store/slices/RenderingSlice"
+    const { status } = useSelector((s: RootState) => s.rendering.snackbar);
 
-const useLogin = (credentials: LoginCredentials): UseLoginHook => {
-    const userKind = useSelector((s: RootState) => s.auth.userKind);
-    const { status, kind } = useSelector((s: RootState) => s.rendering.snackbar);
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
     const dispatch = useDispatch<AppDispatch>();
     const router = useRouter();
 
     const handleLoginResult = (result: AuthenticationSchemaType): void => {
-        const { success } = result
-<<<<<<< HEAD
-        dispatch(currentLoginStatus(success ? "success" : "failed"));
-=======
-        dispatch(enqueueSnackbar({ kind: 'login', status: 'success' }));
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
+        const { success } = result;
+
+        dispatch(enqueueSnackbar({ kind: "login", status: success ? "success" : "failed" }));
 
         if (success) {
             dispatch(loginSuccess());
         }
     };
 
-    const login = async (
-        email: string,
-        password: string
-    ): Promise<AuthenticationSchemaType> => {
-
-        const result = await trpcClient
-            .auth
-            .login
-            .mutate({
-                email: email,
-                password: password
-            });
+    const login = async (email: string, password: string): Promise<AuthenticationSchemaType> => {
+        const result = await trpcClient.auth.login.mutate({
+            email,
+            password,
+        });
         return result;
     };
 
-    const handleSubmit = async (
-        e: React.FormEvent<HTMLFormElement>
-    ): Promise<void> => {
-
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        const { email, password } = credentials;
-        if ((!email) || (!password)) return;
 
-<<<<<<< HEAD
-        dispatch(currentLoginStatus("pending"));
-=======
-        dispatch(enqueueSnackbar({ kind: 'login', status: "pending" }));
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
+        const { email, password } = credentials;
+        if (!email || !password) return;
+
+        dispatch(enqueueSnackbar({ kind: "login", status: "pending" }));
+
         const result = await login(email, password);
         handleLoginResult(result);
-
-    }
-
+    };
 
     useEffect(() => {
-<<<<<<< HEAD
-        if ((userKind === "anonymous") || (loginStatus === "idle")) return;
+        if (userKind === "anonymous" || status === "idle") return;
 
-        if (userKind === "authenticated" && (loginStatus === "success")) {
-            router.push("/")
+        if (userKind === "authenticated" && status === "success") {
+            router.push("/");
         }
-
-    }, [userKind, loginStatus])
-
+    }, [userKind, status, router]);
 
     return {
-        loginStatus,
-=======
-        if ((userKind === "anonymous") || (status === "idle")) return;
-
-        if (userKind === "authenticated" && (status === "success")) {
-            router.push("/")
-        }
-
-    }, [userKind, status])
-
-
-    return {
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
         handleSubmit,
-    }
+    };
 };
 
-export { useLogin }
+export { useLogin };
--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,65 +1,40 @@
 import type { NextResponse } from "next/server";
 import type { LayoutSlotSchemaType } from "@/src/schemas/layoutSlotSchema";
-<<<<<<< HEAD
-import type { SetStateAction } from "react";
-=======
-<<<<<<< Updated upstream
-=======
-import type { SetStateAction } from "react";
-import { SnackbarStatusAndKind } from "../store/slices/RenderingSlice";
->>>>>>> Stashed changes
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
 
 type HealthCheck = { ok: boolean };
 
 type HealthCheckResponse = Promise<NextResponse<HealthCheck>>;
 
-type MountStatus = 'active' | 'idle';
+type MountStatus = "active" | "idle";
 
-type LoadingStatus = 'idle' | 'pending' | 'failed';
+type LoadingStatus = "idle" | "pending" | "failed";
 
-type LoginStatus = 'idle' | 'success' | 'pending' | 'failed';
-
-type RequestStatus = 'idle' | 'success' | 'pending' | 'failed';
+type RequestStatus = "idle" | "success" | "pending" | "failed";
 
 type UsePopulateEventsListHook = {
-    eventLoadingStatus: LoadingStatus
-}
+    eventLoadingStatus: LoadingStatus;
+};
 
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
 type SnackbarMessages = {
     logout: Record<RequestStatus, string>;
     login: Record<RequestStatus, string>;
     newGroup: Record<RequestStatus, string>;
-<<<<<<< HEAD
-    // register?: ...
-};
-
-
-type createSnackbarMessageTypes = {
-    logout: Record<RequestStatus, string>,
-    login: Record<RequestStatus, string>
-}
-
-type UseLoginHook = {
-    loginStatus: RequestStatus,
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>,
-}
-
-=======
 };
 
 type UseLoginHook = {
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>,
-}
+    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+};
 
->>>>>>> Stashed changes
->>>>>>> 97a54ef (rendering pipeline for snackbars + modals in <TopLayerHost/>)
-type EventsPages = Array<LayoutSlotSchemaType[]>
+type EventsPages = Array<LayoutSlotSchemaType[]>;
 
-
-export type { LoadingStatus, UsePopulateEventsListHook, HealthCheck, HealthCheckResponse, MountStatus, EventsPages, UseLoginHook, RequestStatus, SnackbarMessages }
+export type {
+    LoadingStatus,
+    UsePopulateEventsListHook,
+    HealthCheck,
+    HealthCheckResponse,
+    MountStatus,
+    EventsPages,
+    UseLoginHook,
+    RequestStatus,
+    SnackbarMessages,
+};
