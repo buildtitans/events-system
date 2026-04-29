ca552ab004463a03c3bb0795f40183619feac296
Trent Irvin <t.m.irvin1997@gmail.com>
2026-04-29 18:21:40 -0500

    mockup of potentially better api surface for trpc client

---
+++ src/server/core/router/routerV2.ts
@@ -0,0 +1,33 @@
+//import { router } from "@/src/server/core/context/init";
+//import { authRouter } from "./routes/authRouter";
+//import { categoriesRouter } from "./routes/categoriesRouter";
+//import { eventsRouter } from "./routes/eventsRouter";
+//import { groupsRouter } from "./routes/groupsRouter";
+//import { groupMembersRouter } from "./routes/groupMembersRouter";
+//import { eventAttendantsRouter } from "./routes/eventAttendantsRouter";
+//import { notificationsRouter } from "./routes/notificationsRouter";
+//import { usersRouter } from "./routes/usersRouter";
+//import { geoCodingRouter } from "./routes/geoCodingRouter";
+
+// Mock up of potentially better api surface
+
+//export const appRouterV2 = router({
+//  events: {
+//    core: eventsRouter,
+//    attendance: eventAttendantsRouter,
+//  },
+//  groups: {
+//    core: groupsRouter,
+//    members: groupMembersRouter,
+//    categories: categoriesRouter,
+//  },
+//  user: {
+//    core: usersRouter,
+//    notifications: notificationsRouter,
+//  },
+//  auth: authRouter,
+//  addressSearch: geoCodingRouter,
+//});
+//
+//export type AppRouterV2 = typeof appRouterV2;
+//
