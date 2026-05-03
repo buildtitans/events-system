d959288a130a41d51119603c3c3177a27f0b795b
Trent Irvin <t.m.irvin1997@gmail.com>
2026-05-02 20:20:45 -0500

    jest setup file for env loading during tests

--- src/server/jest.setup.ts
+++ src/server/jest.setup.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 process.env.TZ = "UTC";
 
-const envPath = path.resolve(__dirname, ".env.test");
+const envPath = path.resolve(__dirname, ".env");
 
 const result = config({ path: envPath });
 
