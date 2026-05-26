4e88f5c97574417a96ca03e812d7b6eb2506e783
Trent Irvin <t.m.irvin1997@gmail.com>
2026-05-26 13:26:46 -0500

    chore: cleanup on render for opened group events + archives w/exhaustive checks

--- src/client/components/pipelines/groups/displays/renderGroupCalandar.tsx
+++ src/client/components/pipelines/groups/displays/renderGroupCalandar.tsx
@@ -1,31 +1,59 @@
-"use client"
+"use client";
 import type { JSX } from "react";
 import type { FlattenedGroupEventsState } from "@/src/lib/store/slices/groups/OpenedGroupSlice";
 import Calandar from "../../../ui/dates/calandar";
-import NoScheduledEvents from "../../../ui/feedback/info/suggestScheduleEvent";
 import { RelativeSpinner } from "../../../ui/feedback/pending/spinner";
+import FadeIn from "../../../ui/box/motionboxes/fadeIn";
+import AsyncFailedFallback from "../../../ui/feedback/failure/asyncFailedFallback";
+import OpenedGroupFallback from "../../../ui/feedback/fallbacks/groupFallback";
+import { assertNever } from "@/src/lib/utils/assert/assertNever";
 
 type RenderGroupCalandarProps = {
-    flattenedGroupEvents: FlattenedGroupEventsState
-}
+  flattenedGroupEvents: FlattenedGroupEventsState;
+};
 
-export const RenderGroupCalandar = ({flattenedGroupEvents}: RenderGroupCalandarProps): JSX.Element => {
-
-
-    switch(flattenedGroupEvents.status) {
-
-        case "ready": {
-            return (
-                <Calandar history={flattenedGroupEvents.data} />
-            )
-        }
-
-        case "pending": {
-            return <RelativeSpinner />
-        }
-
-        default: {
-          return (  <NoScheduledEvents />)
-        }
+export const RenderGroupCalandar = ({
+  flattenedGroupEvents,
+}: RenderGroupCalandarProps): JSX.Element => {
+  switch (flattenedGroupEvents.status) {
+    case "ready": {
+      return (
+        <FadeIn keyValue="calandar-fadein">
+          <Calandar history={flattenedGroupEvents.data} />
+        </FadeIn>
+      );
     }
-}
+    case "failed": {
+      return (
+        <FadeIn keyValue="failed-fadein">
+          <AsyncFailedFallback />
+        </FadeIn>
+      );
+    }
+    case "n/a": {
+      return (
+        <FadeIn keyValue="n/a-fade-in">
+          <OpenedGroupFallback
+            eyeBrow="Overview"
+            fallbackTitle="No events have been scheduled"
+            fallbackDescripton="This group has not scheduled any events yet, so there are no events to RSVP to right now."
+            fallbackCaption="If you want to get in touch with the organizer, their email is listed above."
+          />
+        </FadeIn>
+      );
+    }
+
+    case "initial":
+    case "pending": {
+      return (
+        <FadeIn keyValue="pending-fadein">
+          <RelativeSpinner />
+        </FadeIn>
+      );
+    }
+
+    default: {
+      return assertNever(flattenedGroupEvents);
+    }
+  }
+};
